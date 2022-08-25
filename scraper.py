68e28a10a482b89e2cc2016c5cacba51387ce253
Luuka Lindgren <luslin@utu.fi>
2022-08-25 13:54:49 +0300

    Scraper ja sen luoma JSON valmiita. Lisätty tiedostoon kommentteja.

--- scraper.py
+++ scraper.py
@@ -1,47 +1,38 @@
-from xml.dom.minidom import Element
-import requests
-import datetime
-import json
-from bs4 import BeautifulSoup
+import requests                     # Tarvitaan nettisivun tietojen hakemiseen
+import datetime                     # Päivämäärää tarvitaan oikeaan URL:iin
+import json                         # Python scriptin tiedot JSON:iin ja sieltä sitten Next.js:lle
+from bs4 import BeautifulSoup       # Tietojen "scrappaamisen" takia
 
 # TÄMÄ PYTHON SCRAPER HAKEE METRIXIN SIVUILTA PÄIVÄN KISAT
-# NYT VAIN TULOSTAA NE KONSOLIIN, MUUTETTAVA SITEN, ETTÄ NEXT.JS
-# SOVELLUS PYSTYY TULOSTAMAAN NÄMÄ NETTISIVULLE.
+# JA LISÄÄ NIISTÄ VALITUT TIEDOT KISAT.JSON -TIEDOSTOON.
 
-days = ["Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai", "Lauantai", "Sunnuntai"]
-date = str(datetime.date.today())
-weekday = days[datetime.date.today().weekday()]
-print("Tänään on:")
-print(weekday, date, end="\n"*2)
+print("Haetaan kisoja...")
 
+date = str(datetime.date.today())               # Kisojen filtteröinti on URL:issa, joten siihen lisätty päivämäärä
 URL = "https://discgolfmetrix.com/competitions_server.php?name=&date1=" + date + "&date2=" + date + "&registration_date1=&registration_date2=&country_code=FI&area=Varsinais-Suomi&from=1&to=20&page=all"
-page = requests.get(URL)
+page = requests.get(URL)                        
 
 soup = BeautifulSoup(page.content, "html.parser")
-kisat = soup.find_all(class_="column")
+kisat = soup.find_all(class_="column")          # Kisat ovat divien alla joiden class="column" 
 
-summa = 0
-kisatDict = {}
-for result in kisat:
-    summa += 1
+määrä = 0
+kisatJson = {"kisat": []}
+for result in kisat:                            # Täytyy käydä sivun HTML läpi, koska 
+    uusiKisa = {}                               # halutuille tiedoille ei ole saatavilla APIA
+    määrä += 1
     kisanNimi = result.find("h2")
     kisanTiedot = result.find("ul", class_="metadata-list")
     kisanAika = kisanTiedot.find("li")
     kisanPaikka = kisanAika.find_next("li")
     kisanAlue = kisanPaikka.find_next("li")     # Sadetutkaa/säätä varten
-    kisatDict["paikka"+str(summa)] = str(kisanPaikka.text.strip())
-    print(kisanPaikka.text.strip(), end="\n")
-    kisatDict["nimi"+str(summa)] = str(kisanNimi.text.strip())
-    print(kisanNimi.text.strip(), end="\n")
-    kisatDict["aika"+str(summa)] = str(kisanAika.text.strip())
-    print(kisanAika.text.strip(), end="\n")
-    kisatDict["alue"+str(summa)] = str(kisanAlue.text.strip())
-    print(kisanAlue.text.strip(), end="\n"*2)
-
-
-print("Kisoja yhteensä:")
-print(summa)
-kisatDict["maara"] = str(summa)
+    uusiKisa["ID"] = määrä
+    uusiKisa["paikka"] = kisanPaikka.text.strip()
+    uusiKisa["nimi"] = kisanNimi.text.strip()   # Poistetaan HTML tagit ja ylimääräiset välit teksteistä
+    uusiKisa["aika"] = kisanAika.text.strip()
+    uusiKisa["alue"] = kisanAlue.text.strip()
+    kisatJson["kisat"].append(uusiKisa)         # Lisätään jokainen kisa omana dictinä
 
 with open("kisat.json", "w") as file:
-    json.dump(kisatDict, file)
+    json.dump(kisatJson, file)                  # Kirjataan haetut kisat oikeassa muodossa JSON:iin
+
+print("Haku onnistui!")
