254a99ba80d48a70af487c83f33e9b525db28e2d
Luuka Lindgren <luslin@utu.fi>
2023-04-01 19:56:08 +0300

    Lisätty MongoDB

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7d1d8cd767090f40"; }
+/******/ 		__webpack_require__.h = function() { return "e1dfd89fbc33e4a6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- scraper.py
+++ scraper.py
@@ -2,15 +2,20 @@ import requests                     # Tarvitaan nettisivun tietojen hakemiseen
 import datetime                     # Päivämäärää tarvitaan oikeaan URL:iin
 import json                         # Python scriptin tiedot JSON:iin ja sieltä sitten Next.js:lle
 from bs4 import BeautifulSoup       # Tietojen "scrappaamisen" takia
+from pymongo import MongoClient     # MongoDB:hen kirjoittamista varten
+import os
+from dotenv import load_dotenv      # Salasanojen piilottamista varten
 
 # TÄMÄ PYTHON SCRAPER HAKEE METRIXIN SIVUILTA PÄIVÄN KISAT
 # JA LISÄÄ NIISTÄ VALITUT TIEDOT data.json -TIEDOSTOON.
 
+load_dotenv()
+
 print("Haetaan kisoja...")
 
 date = str(datetime.date.today())               # Kisojen filtteröinti on URL:issa, joten siihen lisätty päivämäärä
-URL = "https://discgolfmetrix.com/competitions_server.php?name=&date1=" + date + "&date2=" + date + "&registration_date1=&registration_date2=&country_code=FI&area=Varsinais-Suomi&from=1&to=20&page=all"
-page = requests.get(URL)                        
+URL = "https://discgolfmetrix.com/competitions_server.php?name=&date1=" + date + "&date2=" + date + "&country_code=FI&from=1&to=20&page=all" #&area=Varsinais-Suomi
+page = requests.get(URL)                       
 
 soup = BeautifulSoup(page.content, "html.parser") # BeautifulSoup jäsentää HTML:n
 kisat = soup.find_all(class_="column")          # Kisat ovat divien alla joiden class="column" 
@@ -35,4 +40,18 @@ for result in kisat:                            # Täytyy käydä sivun HTML lä
 with open("data.json", "w") as file:
     json.dump(kisatJson, file)                  # Kirjataan haetut kisat oikeassa muodossa JSON:iin
 
-print("Haku onnistui!")
+
+#CONNECTION_STRING = "mongodb+srv://luslin:iFLvPfSCRtB7NJd7@testi.wnkxl99.mongodb.net/Kisakalenteri" 
+client = MongoClient(os.getenv("DATABASE_URL"))        # Yhdistetään MongoDB:hen
+db = client.Kisakalenteri      # Haetaan tietokanta
+coll = db.Kisat                # Haetaan collection
+coll.drop()
+
+result = coll.insert_many(kisatJson["kisat"])    # Lisätään kisat collectioniin
+
+print(result.inserted_ids)
+
+
+print("Kisat lisätty tietokantaan!")
+
+client.close()
